a26850c9a1e67c384a88324a6c0b7e192fe0018b
sathvikkreddy <sathvikreddy8685@gmail.com>
2024-04-19 22:21:41 +0530

    profile_details

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default async function () {
   const balance = await getBalance();
   return (
     <div className="m-2 p-2">
-      <Card title="Profile Details">
+      <Card title="User Details">
         <div className="pt-2">
           <div>Id: {session?.user?.id}</div>
           <div>Name: {session?.user?.name || "NA"}</div>
